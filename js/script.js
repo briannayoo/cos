cf39ec667719b2db4d9a757b00d1e6a9634c0627
briannayoo <briannayoo94@gmail.com>
2024-02-21 20:42:48 +0900

    upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-// const mainMenu = document.querySelectorAll('nav>ul>li');
-// const header = document.querySelector('header');
-// const headerHeight = header.offsetHeight;
+const mainMenu = document.querySelectorAll('#list>li');
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
 
-// for(let mm of mainMenu){
-// 	mm.addEventListener('mouseover',()=>{
-// 		let smHeight = mm.querySelector('ul').offsetHeight;
-// 		let totalHeight = smHeight + headerHeight;
-// 		header.style.height = `${totalHeight}px`;
-// 	});
-// 	mm.addEventListener('mouseout',()=>{
-// 		header.style.height = `${headerHeight}px`;
-// 	});
-// }
+for(let mm of mainMenu){
+	mm.addEventListener('mouseover',()=>{
+		let smHeight = mm.querySelector('ul').offsetHeight;
+		let totalHeight = smHeight + headerHeight;
+		header.style.height = `${totalHeight}px`;
+	});
+	mm.addEventListener('mouseout',()=>{
+		header.style.height = `${headerHeight}px`;
+    });
+};
